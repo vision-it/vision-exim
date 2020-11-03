34a0201795845f7228572eac7d2077c2cbe731f3
Markus Opolka <opolkams@iis.fraunhofer.de>
2020-11-03 11:17:28 +0100

    Add mailalias config

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,11 +5,13 @@
 # ----------
 #
 # @param smarthost Which mailserver to use
+# @param mail_alias Mail alias for root user
 #
 
 class vision_exim::client (
 
-  String $smarthost
+  String $smarthost,
+  String $mail_alias,
 
 ) {
 
@@ -22,6 +24,11 @@ class vision_exim::client (
     content => template('vision_exim/exim4-client.conf.erb'),
     require => Package['exim4'],
     notify  => Service['exim4'],
+  }
+
+  file { '/etc/email-addresses':
+    ensure  => present,
+    content => "root: ${mail_alias}",
   }
 
   file { '/etc/mailname':
--- manifests/server.pp
+++ manifests/server.pp
@@ -6,12 +6,14 @@
 #
 # @param smarthost Which mailserver to use
 # @param relay_nets From what clients to accept mails
+# @param mail_alias Mail alias for root user
 #
 
 class vision_exim::server (
 
   String $smarthost,
   String $relay_nets,
+  String $mail_alias,
 
 ) {
 
@@ -24,6 +26,11 @@ class vision_exim::server (
     content => template('vision_exim/exim4-server.conf.erb'),
     require => Package['exim4'],
     notify  => Service['exim4'],
+  }
+
+  file { '/etc/email-addresses':
+    ensure  => present,
+    content => "root: ${mail_alias}",
   }
 
   file { '/etc/mailname':
